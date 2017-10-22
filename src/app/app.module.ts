e008949c0b323f64ee65186150494911053c745b
commandlinedesign <commandlinedesign@gmail.com>
2017-10-21 22:41:41 -0700

    implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { NgBoxModule } from 'ngbox/ngbox.module';
+import { NgBoxService } from 'ngbox/ngbox.service';
+import { CommonModule } from '@angular/common';
+
 import { CoreModule } from "./core/core.module";
 
 import { AppComponent } from "./app.component";
@@ -11,13 +15,17 @@ import { GalleryComponent } from "./gallery/gallery.component";
 @NgModule({
     imports: [
       BrowserModule,
+      NgBoxModule,
+      BrowserModule,
+      CommonModule
     ],
     declarations: [
       AppComponent,
       NavComponent,
       TitleComponent,
       GalleryComponent
-   ],
+    ],
+    providers: [ NgBoxService ],
     bootstrap: [ AppComponent],
     entryComponents: [ AppComponent ]
 })
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 
 import { CoreModule } from "../core/core.module";
 
 @NgModule({
-    imports: [CoreModule]
+    imports: [CoreModule, CommonModule]
 })
 export class GalleryModule {}
